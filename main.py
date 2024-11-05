84e4548e1e528f7edf0ded59c699ada6f73542e0
SaadNabeel <saadnabeel2010@gmail.com>
2024-11-05 18:49:25 +0500

    commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 class Vehicle:
 
-    def __init__(self, name, max_speed, mileage):
+    def __init__(self, name, max_speed, mileage, owner):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
-
+        self.owner=owner
 class Bus(Vehicle):
     pass
 
-Schoolbus = Bus("School Volvo", 180, 12)
+Schoolbus = Bus("School Volvo", 180, 12,"Saad")
 
-print("Vehicle Name:", Schoolbus.name, "Speed:", Schoolbus.max_speed, "Mileage:", Schoolbus.mileage)
+print("Vehicle Name:", Schoolbus.name, "Speed:", Schoolbus.max_speed, "Mileage:", Schoolbus.mileage,'Owner:',Schoolbus.owner)
